4af1196729d51e8301bbbf427dcaa8f6c3ff4a1b
admhabits <admhabits@gmail.com>
2021-08-27 03:58:49 +0700

    add files src, slicing, produksi

---
+++ produksi/config/FIREBASE/index.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDlDEyuD1dlqoH8xYsuaj44gtWnNttWNYw",
+    authDomain: "vitr-tv.firebaseapp.com",
+    databaseURL: "https://vitr-tv-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "vitr-tv",
+    storageBucket: "vitr-tv.appspot.com",
+    messagingSenderId: "887950662893",
+    appId: "1:887950662893:android:c0386c3de8ea6c69fe40e4",
+    measurementId: "G-QV6W5V8542"
+});
+
+const FIREBASE = firebase;
+
+export default FIREBASE
